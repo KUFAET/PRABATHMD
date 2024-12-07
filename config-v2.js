426bda24368aaf8528041de91b4f68121bc41faa
KUFAET <kufaethan12@gmail.com>
2024-12-07 12:51:08 +0200

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const BOT_NUMBER = "263 71 803 6108"
+const SESSION_ID = "PRABATH-MD~qAslGbgA#jajAZQJXt4IydHWBOxxhrnu7labSAttYNX5L9YEGMbI"
 
 
 module.exports = {
